408ecf684cc054749f9557b5a75a4128a2c35371
NC <nicolas.candeli@cegetel.net>
2022-01-02 13:48:33 +0100

    Révision des conditions de la recherche principale

--- js/recherche.js
+++ js/recherche.js
@@ -10,6 +10,9 @@ export const champsPrincipal = document.querySelector('#rech-princ');
 export let recettesFiltreesParChampsPrincipal;
 export let recettesFiltreesParMotsCles;
 
+// Autre
+let recettesNonFiltreesAffichees = false; /* Variable contrôlant l'affichage des recettes non filtrées */
+
 // Classe de recherche classique
 export class Recherche {
   // recettes = recettes;
@@ -21,48 +24,96 @@ export class Recherche {
   // Filtre les recettes correspondant à la valeur du champs (affiche les recettes et les copie dans "recettesFiltreesParChampsPrincipal")
   filtreRecettesParChampsPrincipal(recettes) {
     const entree =  Utilitaire.harmonise(this.champs.value);
-    this.conteneur.innerHTML = '';
-    recettesFiltreesParChampsPrincipal = [];
   
-    // Si le champs est composé d'au moins 3 caractères et qu'aucun mot-clé n'a été sélectionné ...
-    if ((entree.length >= 3) && (motsClesChoisis.length === 0)) {
-      for (let recette of recettes) {
-        if ((this.verifieChaine(entree, recette, 'name'))
-          || (this.verifieListe(entree, recette, 'pureIngredients'))
-          || (this.verifieListe(entree, recette, 'ustensils'))
-          || (this.verifieListe(entree, recette, 'appliance'))
-          || (this.verifieChaine(entree, recette, 'description'))) {
+    // // Si le champs est composé d'au moins 3 caractères et qu'aucun mot-clé n'a été sélectionné ...
+    // if ((entree.length >= 3) && (motsClesChoisis.length === 0)) {
+    //   for (let recette of recettes) {
+    //     if ((this.verifieChaine(entree, recette, 'name'))
+    //       || (this.verifieListe(entree, recette, 'pureIngredients'))
+    //       || (this.verifieListe(entree, recette, 'ustensils'))
+    //       || (this.verifieListe(entree, recette, 'appliance'))
+    //       || (this.verifieChaine(entree, recette, 'description'))) {
+    //       affichage.inscritRecettes(recette);
+    //       recettesFiltreesParChampsPrincipal.push(recette);
+    //     }
+    //   }
+    // }
+    // // Si le champs est composé de moins de 3 caractères et qu'aucun mot-clé n'a été sélectionné ...
+    // else if ((entree.length < 3) && (motsClesChoisis.length === 0)) {
+    //   for (let recette of recettes) {
+    //     affichage.inscritRecettes(recette);
+    //     recettesFiltreesParChampsPrincipal.push(recette);
+    //   }
+    // }
+    // // Si le champs est composé d'au moins 3 caractères et qu'un mot-clé a déjà été sélectionné ...
+    // else if ((entree.length >= 3) && (motsClesChoisis.length > 0)) {
+    //   for (let recette of recettes) {
+    //     if ((this.verifieChaine(entree, recette, 'name'))
+    //       || (this.verifieListe(entree, recette, 'pureIngredients'))
+    //       || (this.verifieListe(entree, recette, 'ustensils'))
+    //       || (this.verifieListe(entree, recette, 'appliance'))
+    //       || (this.verifieChaine(entree, recette, 'description'))) {
+    //       recettesFiltreesParChampsPrincipal.push(recette);
+    //     }
+    //   }
+    //   this.filtreRecettesParMotsCles(motsClesChoisis);
+    // }
+    // Si le champs est composé de moins de 3 caractères et qu'un mot-clé a déjà été sélectionné ...
+    // else if ((entree.length < 3) && (motsClesChoisis.length > 0)) {
+    //   for (let recette of recettes) {
+    //     recettesFiltreesParChampsPrincipal.push(recette);
+    //   }
+    //   this.filtreRecettesParMotsCles(motsClesChoisis);
+    // }
+
+    // Si le champs est composé de moins de 3 caractères et que les recettes non filtrées n'ont pas déjà été affichées
+    if ((entree.length < 3) && (!recettesNonFiltreesAffichees)) {
+      recettesFiltreesParChampsPrincipal = [];
+      recettesNonFiltreesAffichees = true;
+      // Si la liste de mots-clés est vide ...
+      if (motsClesChoisis.length === 0) {
+        this.conteneur.innerHTML = '';
+        for (let recette of recettes) {
           affichage.inscritRecettes(recette);
           recettesFiltreesParChampsPrincipal.push(recette);
         }
-      }
-    }
-    // Si le champs est composé de moins de 3 caractères et qu'aucun mot-clé n'a été sélectionné ...
-    else if ((entree.length < 3) && (motsClesChoisis.length === 0)) {
-      for (let recette of recettes) {
-        affichage.inscritRecettes(recette);
-        recettesFiltreesParChampsPrincipal.push(recette);
-      }
-    }
-    // Si le champs est composé d'au moins 3 caractères et qu'un mot-clé a déjà été sélectionné ...
-    else if ((entree.length >= 3) && (motsClesChoisis.length > 0)) {
-      for (let recette of recettes) {
-        if ((this.verifieChaine(entree, recette, 'name'))
-          || (this.verifieListe(entree, recette, 'pureIngredients'))
-          || (this.verifieListe(entree, recette, 'ustensils'))
-          || (this.verifieListe(entree, recette, 'appliance'))
-          || (this.verifieChaine(entree, recette, 'description'))) {
+      // Si la liste de mots-clés n'est pas vide ...
+      } else {
+        for (let recette of recettes) {
           recettesFiltreesParChampsPrincipal.push(recette);
         }
+        this.filtreRecettesParMotsCles(motsClesChoisis);
       }
-      this.filtreRecettesParMotsCles(motsClesChoisis);
-    }
-    // Si le champs est composé de moins de 3 caractères et qu'un mot-clé a déjà été sélectionné ...
-    else if ((entree.length < 3) && (motsClesChoisis.length > 0)) {
-      for (let recette of recettes) {
-        recettesFiltreesParChampsPrincipal.push(recette);
+    // Si le champs est composé d'au moins 3 caractères ...
+    } else {
+      recettesFiltreesParChampsPrincipal = [];
+      recettesNonFiltreesAffichees = false;
+      // Si la liste de mots-clés est vide ...
+      if (motsClesChoisis.length === 0) {
+        this.conteneur.innerHTML = '';
+        for (let recette of recettes) {
+          if ((this.verifieChaine(entree, recette, 'name'))
+            || (this.verifieListe(entree, recette, 'pureIngredients'))
+            || (this.verifieListe(entree, recette, 'ustensils'))
+            || (this.verifieListe(entree, recette, 'appliance'))
+            || (this.verifieChaine(entree, recette, 'description'))) {
+            affichage.inscritRecettes(recette);
+            recettesFiltreesParChampsPrincipal.push(recette);
+          }
+        }
+      // Si la liste de mots-clés n'est pas vide ...
+      } else {
+        for (let recette of recettes) {
+          if ((this.verifieChaine(entree, recette, 'name'))
+            || (this.verifieListe(entree, recette, 'pureIngredients'))
+            || (this.verifieListe(entree, recette, 'ustensils'))
+            || (this.verifieListe(entree, recette, 'appliance'))
+            || (this.verifieChaine(entree, recette, 'description'))) {
+            recettesFiltreesParChampsPrincipal.push(recette);
+          }
+        }
+        this.filtreRecettesParMotsCles(motsClesChoisis);
       }
-      this.filtreRecettesParMotsCles(motsClesChoisis);
     }
     console.log(recettesFiltreesParChampsPrincipal.length);
   }
